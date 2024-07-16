8e798c27ef86bd4fd029fbe2011023b0df4bc441
Sonu2901 <108445636+Sonu2901@users.noreply.github.com>
2024-07-16 11:13:19 +0530

    Update customer.js

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const router = express.Router();
 const customerController = require('../controllers/customerController');
-
+const validator=require('../middleware/validator.js');
 /**
  *  Customer Routes 
 */
 router.get('/', customerController.homepage);
 router.get('/about', customerController.about);
 router.get('/add', customerController.addCustomer);
-router.post('/add', customerController.postCustomer);
+router.post('/add',validator.valiDate, customerController.postCustomer);
 router.get('/view/:id', customerController.view);
 router.get('/edit/:id', customerController.edit);
 router.put('/edit/:id', customerController.editPost);
